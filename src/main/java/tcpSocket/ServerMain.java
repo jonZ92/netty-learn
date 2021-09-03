058cb4c69c544b9e3a68e7651f6a0fc706057e93
jon-yy <zjon234@gmail.com>
2021-09-04 01:12:51 +0800

    first commit

---
+++ src/main/java/tcpSocket/ServerMain.java
@@ -0,0 +1,13 @@
+package tcpSocket;
+
+/**
+ * @author jon 2021:09:04
+ */
+
+
+public class ServerMain {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/tcpSocket/server/TcpServerSocket.java
@@ -0,0 +1,14 @@
+package tcpSocket.server;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author jon 2021:09:04
+ */
+
+
+public class TcpServerSocket {
+    private final Logger log = LogManager.getLogger(this.getClass());
+
+}
